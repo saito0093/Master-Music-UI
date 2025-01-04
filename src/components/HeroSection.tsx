69d4a7aab2f99a86f5d3954b7d24cc8a14ced7d1
yuyasano0096 <mackbus0096@gmail.com>
2025-01-04 17:40:50 +0530

    fix

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,7 +13,7 @@ function HeroSection() {
         />
        <div className='p-4 relative z-10 w-full text-center'>
          <h1 className="mt-20 md:mt-0 text-8xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music </h1>
-         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or a seasoned musician, our courses are designed to empower you with the knowledge and skills to excel in the world of music.</p>
+         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into our comprehensive music courses and transform your musical journey today. Whether you are a beginner or a seasoned musician, our courses are designed to empower you with the knowledge and skills to excel in the world of music.</p>
          <div className='mt-4'>
             <Link href ={"/courses"}>
                <Button 
--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -123,7 +123,7 @@ export const CardItem = ({
 
   useEffect(() => {
     handleAnimations();
-  }, [isMouseEntered]);
+  }, [isMouseEntered ]);
 
   const handleAnimations = () => {
     if (!ref.current) return;
